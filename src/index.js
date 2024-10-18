99dd28ea15e165356f31844a884f219d89c7335d
engProgrammer2001 <askushwaha613@gmail.com>
2024-10-18 11:45:09 +0530

    change origin

--- src/index.js
+++ src/index.js
@@ -8,49 +8,55 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 import resumeRoute from "./routes/addresume.route.js";
+
 dotenv.config({});
 
 const app = express();
 
+// Base route
 app.get("/", (req, res) => {
-  return res
-    .status(200)
-    .send({ message: "welcome to our job portal api - node ", status: true });
+  return res.status(200).send({ 
+    message: "Welcome to our job portal API - Node.js", 
+    status: true 
+  });
 });
 
 app.use('/uploads', express.static('uploads'));
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
 app.use(cookieParser());
 
-// cors origin policy
 const corsOptions = {
-  origin: "https://skokka.org.in/",
-  credentials: true,
+  //  origin: "https://skokka.org.in",
+   origin: "https://skokka.org.in",
+  credentials: true, 
 };
+
 app.use(cors(corsOptions));
+
+// Handling preflight OPTIONS request
+app.options('*', cors(corsOptions)); 
+
 const PORT = process.env.PORT || 5454;
 
-// register and login route 
+// Register and login route
 app.use("/user", userRoute);
 
-// add company routes
+// Add company routes
 app.use("/company", companyRoute);
 
 app.use("/job", jobRoute);
 
-// application routes 
+// Application routes 
 app.use("/application", applicationRoute);
 
-// add resume routes
+// Add resume routes
 app.use("/resume", resumeRoute);
 
-
-
+// Start the server
 app.listen(PORT, () => {
-    connectDB();
-  console.log(`server is running on port ${PORT}`);
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
